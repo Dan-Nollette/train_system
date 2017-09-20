71b8307cfaa1d3893a08ad65777fc0913516ff65
Dan Nollette and Linda Luu <linda@>
2017-09-20 16:25:33 -0700

    add functionality to user interface and allow user to CRUD

--- app.rb
+++ app.rb
@@ -22,12 +22,73 @@ get('/operator/city/list') do
   erb(:cities)
 end
 
-get('/operator/trains') do
+get('/operator/trains/list') do
   @trains = Train.all
   erb(:trains)
 end
 
 get('/operator/city/new') do
   @cities = City.all
-  erb(:city/new)
+  erb(:city_new)
+end
+
+get('/operator/train/new') do
+  @trains = Train.all
+  erb(:train_new)
+end
+
+post('/operator/city/list') do
+  city_name = params.fetch(:city_name)
+  City.new(id: nil, name: city_name).save
+  @cities = City.all
+  erb(:cities)
+end
+
+post('/operator/train/list') do
+  train_number = params.fetch(:train_number)
+  nickname = params.fetch(:nickname)
+  Train.new(train_number: train_number, nickname: nickname, id: nil).save
+  @trains = Train.all
+  erb(:trains)
+end
+
+delete("/operator/city/list/:id") do
+  @city = City.find(params.fetch("id").to_i())
+  @city.delete()
+  @cities = City.all()
+  erb(:cities)
+end
+
+delete("/operator/train/list/:id") do
+  @train = Train.find(params.fetch("id").to_i())
+  @train.delete()
+  @trains = Train.all()
+  erb(:trains)
+end
+
+patch("/operator/city/list/:id") do
+  name = params.fetch("name")
+  @city = City.find(params.fetch("id").to_i())
+  @city.update({:name => name})
+  @cities = City.all
+  erb(:cities)
+end
+
+patch("/operator/train/list/:id") do
+  train_number = params.fetch("train_number")
+  nickname = params.fetch("nickname")
+  @train = Train.find(params.fetch("id").to_i())
+  @train.update({:train_number => train_number, :nickname => nickname})
+  @trains = Train.all
+  erb(:trains)
+end
+
+get('/operator/city/city_edit/:id') do
+  @city = City.find(params.fetch("id").to_i())
+  erb(:city_edit)
+end
+
+get('/operator/train/train_edit/:id') do
+  @train = Train.find(params.fetch("id").to_i())
+  erb(:train_edit)
 end
